56e13f8aecba81ea70e2e02b93c3c30497786126
Fractal Feeling <kevin.mcisaac@gmail.com>
2021-07-26 08:29:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     name="CWT", 
     version=2,
     description="A tensorflow 2.0 Continuous Wavelet Transform",
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     packages=['CWT'],
     install_requires=['numpy', 'tensorflow'],
     python_requires='>=3.6',
